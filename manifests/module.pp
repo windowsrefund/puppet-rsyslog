703a19dad7718e5bcae056e5de0117f3d1cff872
windowsrefund <windowsrefund@gmail.com>
2011-05-23 18:12:01 -0400

    cleanup

--- manifests/module.pp
+++
@@ -1,8 +0,0 @@
-define rsyslog::module($content = undef, $ensure = present) {
-
-	rsyslog::config { "module-$name": 
-		ensure => $ensure,
-		content => "\$ModLoad $name\n$content", 
-	}
-
-}
--- manifests/rule/remotehost.pp
+++
@@ -1,7 +0,0 @@
-class rsyslog::rule::remotehost{
-
-	rsyslog::config { 'rule-remotehost':
-		content => "\$template RemoteHost, \"/var/log/hosts/%HOSTNAME%/%\$YEAR%/%\$MONTH%/%\$DAY%/%syslogfacility-text%.log\"\nif \$source != 'localhost' then ?RemoteHost\n" 
-	}
-
-}
